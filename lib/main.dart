73567a85807a7e03c9b2e5f0c4a60b34cce43e6d
daniel.stuewe <daniel.stuewe@maibornwolff.de>
2024-06-21 12:51:56 +0000

    add amplify as dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,33 @@
+import 'package:amplify_api/amplify_api.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
 
 import 'models/room.dart';
 import 'room_page.dart';
 
 void main() {
+  _configureAmplify();
   runApp(const MyApp());
+}
+
+Future<void> _configureAmplify() async {
+  try {
+    await Amplify.addPlugins(
+      [
+        AmplifyAuthCognito(),
+        AmplifyAPI(
+          options: APIPluginOptions(
+            modelProvider: ModelProvider.instance,
+          ),
+        ),
+      ],
+    );
+    await Amplify.configure(amplifyConfig);
+    safePrint('Successfully configured');
+  } on Exception catch (e) {
+    safePrint('Error configuring Amplify: $e');
+  }
 }
 
 class MyApp extends StatelessWidget {
